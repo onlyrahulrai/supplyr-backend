dcac594ff34a361d5b4fb55ee44e5a31fc1b6891
AbuTalha Danish <a.danish@string.org.in>
2020-12-09 01:36:26 +0530

    Wsgi file fix

--- supplyr/wsgi.py
+++ supplyr/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supplyr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supplyr.settings_local')
 
 application = get_wsgi_application()
