bbd5e9b17e47f428960bc7c054ae9940695b75a1
Rahul Rai <rrai06125@gmail.com>
2021-11-23 20:03:29 +0530

    a thread has been used for sending mail in the background.

--- supplyr/core/auth.py
+++ supplyr/core/auth.py
@@ -1,8 +1,10 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
+import threading
 
 class CustomAccountAdapter(DefaultAccountAdapter):
 
     def send_mail(self, template_prefix, email, context):
         context['activate_url'] = settings.URL_FRONTEND + 'confirm-email/' + context['key']
-        return super().send_mail(template_prefix, email, context)
+        mailing_thread = threading.Thread(target=super(CustomAccountAdapter,self).send_mail,args=(template_prefix,email,context))
+        mailing_thread.start()
